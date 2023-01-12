01defbae43212a73ace2c34d138e8de19aa772cd
Tony Dandino <tdandino1005@gmail.com>
2023-01-11 21:44:51 -0600

    updated all files

--- app/index.js
+++ app/index.js
@@ -1,1 +1,20 @@
-export const add2Nums = (num1, num2) => num1 + num2;
+import express from "express";
+import config from "./config.js";
+import userRouter from "./user/routes.js";
+
+const app = express();
+
+// Logical OR short-circuits if the first value is truthy
+const PORT = config.port || 3000;
+
+app.use(express.json());
+
+app.use("/api/users", userRouter);
+
+app.use((_, res) => {
+  res.status(404).json({ error: "Not found" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
